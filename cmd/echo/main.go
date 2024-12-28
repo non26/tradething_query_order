325a66475c4da84941c5e74b24d450a44099864c
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2024-12-28 15:56:14 +0700

    feat: bn position

---
+++ cmd/echo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"tradethingqueryorder/cmd/route"
+	"tradethingqueryorder/config"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	config, err := config.ReadConfig()
+	if err != nil {
+		fmt.Println("error read config", err)
+		return
+	}
+
+	app_echo := echo.New()
+	route.Compose(app_echo, config)
+	app_echo.Start(fmt.Sprintf(":%v", "8080"))
+}
